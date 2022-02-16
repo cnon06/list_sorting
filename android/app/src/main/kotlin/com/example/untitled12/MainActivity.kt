b25c3c82c7c6aafd3df3fe2ad5dcb41eeabaae8e
sinan <sinanalagoz74@gmail.com>
2022-02-16 10:29:16 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/untitled12/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.untitled12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list.dart
@@ -0,0 +1,9 @@
+class Class_a
+{
+
+String ad;
+String soyad;
+String id;
+
+Class_a(this.ad, this.soyad, this.id);
+}
